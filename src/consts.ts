e62bc11ed3bdb48a763e4b6cc22b438b0829da03
PushToTalk <x_killer90@mail.ru>
2023-05-10 14:44:57 +0300

    Adding page

--- src/consts.ts
+++
@@ -1,5 +0,0 @@
-import { arrNumber } from "./helper";
-
-export const tower = ["А", "Б"];
-export const floor = arrNumber(3, 27);
-export const room = arrNumber(1, 10);
--- src/helper.ts
+++
@@ -1,7 +0,0 @@
-export function arrNumber(startNumber: number, endNumber: number): number[] {
-  const arr = [];
-  for (let i = startNumber; i <= endNumber; i++) {
-    arr.push(i);
-  }
-  return arr;
-}
